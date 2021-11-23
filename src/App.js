2e4dfb25a24adf8b8b75f5a5e75b8d1ceb839ac0
BurningAlfalfa <acmonahan@gmail.com>
2021-11-23 17:54:47 -0600

    added.includes

--- src/App.js
+++ src/App.js
@@ -1,60 +1,63 @@
-import "./App.css";
-import React, { Component } from 'react'
-import * as BooksAPI from './BooksAPI';
+mport "./App.css";
+import React, { Component } from "react";
+import * as BooksAPI from "./BooksAPI";
 //export default class Searchbar extends React.Component
 class App extends Component {
   constructor(props) {
     super();
     this.state = {
-      books: [], // object of title, author , catagorty
+      books: [], // object of title, author , categories
       query: "",
       booksOnDisplay: [],
     };
   }
-  
-  searchBooks = (e)=>{
-    this.setState({[e.target.name]:e.target.value
-    })
-  }
-  submitSearch=(event)=>{
-    const filteredArray = this.books.filter(element => element.includes(this.query))
-  }
+
+  handleChange = (e) => {
+    this.setState({ query: e.target.value });
+  };
+  submitSearch = (event) => {};
   componentDidMount() {
-    BooksAPI.getAll().then(allBooks => {
+    BooksAPI.getAll().then((allBooks) => {
       this.setState({
-    	booksOnDisplay: allBooks.filter(book => book.shelf !== 'none')
-	  })
-  	})
+        booksOnDisplay: allBooks.filter((book) => book.shelf !== "none"),
+      });
+    });
   }
 
-  updateQuerey(query){
-    this.setState({query})
+  updateQuerey(query) {
+    this.setState({ query });
   }
 
- searchBooks(query){
-  BooksAPI.search(query).then((results)=>{
-    if(results && results.length > 0) {
+  searchBooks(query) {
+    BooksAPI.search(query).then((results) => {
+      if (results && results.length > 0) {
         let searchResults = results;
-        searchResults.map((book) => book.shelf ="none")
-        this.state.search.map((book) => {
-          
-        })
-    }
-  })
-   
- }
+        searchResults.map((book) => (book.shelf = "none"));
+        this.state.search.map((book) => {});
+      }
+    });
+  }
 
   render() {
-  return (
-  <div>
-    <input onChange={this.filteredArray} defaultValue={this.state.query}></input>
-    <button onClick ={this.filteredArray}>Search</button>
-  </div>
-  );
+    const filteredArray = this.state.books.filter.ma(book) =>
+    
+      //book.includes(this.state.query)
+      {
+        if (book.author.includes(this.state.query)) {
+          return true;
+        }
+      }
+
+    
+      
+    
+    return (
+      <div>
+        <input onChange={this.handleChange} value={this.state.query}></input>
+        <button onClick={this.filteredArray}>Search</button>
+      </div>
+    );
   }
 }
 
 export default App;
-
-
-
